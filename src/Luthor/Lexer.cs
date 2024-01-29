68f39e09b4df8137f261fe0fb38c03b3cdcb87ff
Mark Lauter <mark@marklauter.com>
2024-01-29 13:33:29 -0500

    renamed methods and added xml summary

--- src/Luthor/Lexer.cs
+++ src/Luthor/Lexer.cs
@@ -4,23 +4,28 @@ using System.Runtime.CompilerServices;
 
 namespace Luthor;
 
+/// <summary>
+/// Mutable OO style lexer that maintains state internally.
+/// </summary>
+/// <param name="context">the linguistic context that defines the regex and scan precedence</param>
+/// <param name="source">the source to be parsed</param>
 public sealed class Lexer(
     LinguisticContext context,
     string source)
 {
     private int position;
     private int lastNewLineOffset;
-    private int line;
+    private int line = 1;
     private readonly LinguisticContext context = context ?? throw new ArgumentNullException(nameof(context));
     private readonly string source = source ?? throw new ArgumentNullException(nameof(source));
 
     public IEnumerable<Token> ReadTokens()
     {
-        var token = ReadNextToken();
+        var token = ReadToken();
         yield return token;
         while (token.Type != TokenType.Eof)
         {
-            token = ReadNextToken();
+            token = ReadToken();
             yield return token;
         };
     }
@@ -31,7 +36,7 @@ public sealed class Lexer(
         return position >= source.Length;
     }
 
-    public Token ReadNextToken()
+    public Token ReadToken()
     {
         if (IsEndOfSource())
         {
@@ -39,7 +44,7 @@ public sealed class Lexer(
         }
 
         var length = context.Length;
-        var languages = context.AsSpan();
+        var languages = context.AsReadOnlySpan();
         for (var i = 0; i < length; ++i)
         {
             var language = languages[i];
