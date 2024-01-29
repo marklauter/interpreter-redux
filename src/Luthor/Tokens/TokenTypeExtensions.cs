29708c45eb4b67734abc43984150b18aba7e8830
Mark Lauter <mark@marklauter.com>
2024-01-29 13:32:55 -0500

    added token type extensions

---
+++ src/Luthor/Tokens/TokenTypeExtensions.cs
@@ -0,0 +1,29 @@
+﻿namespace Luthor.Tokens;
+
+public static class TokenTypeExtensions
+{
+    public static bool IsNaturalDelimiter(this TokenType type)
+    {
+        return type.HasFlag(TokenType.NaturalDelimiter);
+    }
+
+    public static bool IsSimpleName(this TokenType type)
+    {
+        return type.HasFlag(TokenType.SimpleName);
+    }
+
+    public static bool IsLiteral(this TokenType type)
+    {
+        return type.HasFlag(TokenType.Literal);
+    }
+
+    public static bool IsGlyph(this TokenType type)
+    {
+        return type.HasFlag(TokenType.Glyph);
+    }
+
+    public static bool IsOperator(this TokenType type)
+    {
+        return type.HasFlag(TokenType.Operator);
+    }
+}
