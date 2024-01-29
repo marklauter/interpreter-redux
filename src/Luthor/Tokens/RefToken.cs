0d82fe2dfc2de4aefe50078f27c6f68d06ff1df9
Mark Lauter <mark@marklauter.com>
2024-01-29 13:31:20 -0500

    added ref token for use by functional lexer

---
+++ src/Luthor/Tokens/RefToken.cs
@@ -0,0 +1,14 @@
+﻿namespace Luthor.Tokens;
+
+[System.Diagnostics.CodeAnalysis.SuppressMessage("Design", "CA1051:Do not declare visible instance fields", Justification = "it's a struct")]
+public readonly ref struct RefToken(
+    int offset,
+    int length,
+    TokenType type,
+    string value)
+{
+    public readonly int Offset = offset;
+    public readonly int Length = length;
+    public readonly TokenType Type = type;
+    public readonly string Value = value;
+}
