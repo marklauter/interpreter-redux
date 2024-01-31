ab59f498cf37d5104a47e14ac75fc46f12da8b51
Mark Lauter <mark@marklauter.com>
2024-01-31 00:22:13 -0500

    removed comments

--- src/Luthor/Lexer.cs
+++ src/Luthor/Lexer.cs
@@ -3,20 +3,9 @@ using System.Runtime.CompilerServices;
 
 namespace Luthor;
 
-/// <summary>
-/// Immutable functional style lexer that maintains state on the stack.
-/// </summary>
-/// <param name="context">the linguistic context that defines the regex and scan precedence</param>
-// todo: Though the Flexer is slower in unit tests, the expectation is that the FLexer will be faster than the Lexer in real-world use cases because it never touches the heap. Will need to add bench to prove this with a parser.
 public readonly ref struct Lexer(LexicalContext Context)
 {
     private readonly ReadOnlySpan<TokenReader> readers = Context.AsReadOnlySpan();
-
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public ReadTokenResult ReadToken(string source)
-    {
-        return ReadToken(source, 0, 0, 1);
-    }
 
     public ReadTokenResult ReadToken(string source, int offset, int lastNewLineOffset, int lineNumber)
     {
@@ -38,5 +27,11 @@ public readonly ref struct Lexer(LexicalContext Context)
             offset,
             lastNewLineOffset,
             lineNumber);
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public ReadTokenResult ReadToken(string source)
+    {
+        return ReadToken(source, 0, 0, 1);
     }
 }
