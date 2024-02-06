5366acbffd7bec60859e0afeab1fee369166d828
Mark Lauter <mark@marklauter.com>
2024-02-06 01:29:44 -0500

    added math cli project

---
+++ src/Math/Program.cs
@@ -0,0 +1,22 @@
+﻿using Math.Parser;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+using var host = Host
+    .CreateDefaultBuilder(args)
+    .ConfigureServices(services => services.AddParser())
+    .Build();
+
+var parser = host.Services.GetRequiredService<Parser>();
+
+Console.Write("math:> ");
+var source = Console.ReadLine();
+while (!String.IsNullOrWhiteSpace(source))
+{
+    var expression = parser.Parse(source);
+    var result = expression.Evaluate();
+    Console.WriteLine($"result: {result}");
+
+    Console.Write("math:> ");
+    source = Console.ReadLine();
+}
