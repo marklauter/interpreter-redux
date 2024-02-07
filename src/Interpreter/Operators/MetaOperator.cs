6d3fc2c089d2b949d7c5aeb4f70f016e09fd1fa8
Mark Lauter <mark@marklauter.com>
2024-02-07 00:59:31 -0500

    removed dead code

--- src/Interpreter/Operators/MetaOperator.cs
+++
@@ -1,9 +0,0 @@
-﻿//namespace Interpreter.Operators;
-
-//internal abstract class MetaOperator<TResult, TParam>
-//{
-//    public virtual int Precedence { get; }
-//    public virtual int OperandCount { get; }
-//    public virtual string Symbol { get; } = String.Empty;
-//    public abstract TResult Evaluate(params TParam[] args);
-//}
