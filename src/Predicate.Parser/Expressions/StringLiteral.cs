ce3a7bc2a121fb44efc4e2f7b2cdb344c52a221a
Mark Lauter <mark@marklauter.com>
2024-02-11 22:03:39 -0500

    removed quotes from string literal

--- src/Predicate.Parser/Expressions/StringLiteral.cs
+++ src/Predicate.Parser/Expressions/StringLiteral.cs
@@ -14,8 +14,15 @@ public sealed record StringLiteral(
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static explicit operator StringLiteral(string value)
     {
-        // todo: remove the quotes from the string
-        return new(value);
+        var start = value.StartsWith('"')
+            ? 1
+            : 0;
+
+        var end = value.EndsWith('"')
+            ? value.Length - 1
+            : value.Length;
+
+        return new(value[start..end]);
     }
 
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
